42b31377f273b52a0f5e9ec5d8f52ea5b60ac353
Евгений-ПК\Евгений <jeniamasarn94@gmail.com>
2018-03-25 23:44:45 +0300

    +changes

--- NEW.W.2018.Masarnouski.03/NEW.W.2018.Masarnouski.03/Program.cs
+++ NEW.W.2018.Masarnouski.03/NEW.W.2018.Masarnouski.03/Program.cs
@@ -13,37 +13,6 @@ namespace NEW.W._2018.Masarnouski._03
     {
         static void Main(string[] args)
         {
-
-            //double i = -255.255;
-
-            //Int64 temp = Convert.ToInt64(i);
-            //Console.WriteLine(temp);
-
-            //string result = Convert.ToString(temp, 2);
-            //Console.WriteLine(result);
-
-            //var bites = new BitArray(BitConverter.GetBytes(-255.255));
-            //foreach (var item in bites)
-            //{
-            //    Console.Write(item + " , ");
-            //}
-            //Console.ReadLine();
-
-            double x = -255.255;
-            Console.Write(x.ToIEEE754Format());
-            Console.ReadLine();
-            //TimeSpan time = new TimeSpan();
-
-            //Console.WriteLine(NodCount.Evklid(out time, 0, 8, 4));
-            //Console.WriteLine(time);
-            //Console.Write(time.TotalMilliseconds);
-            //Console.ReadLine();
-            //Console.Write(time.TotalMilliseconds>0);
-            //Console.ReadLine();
-            //Console.WriteLine(NodCount.Stein_NOD(0,750,780));
-            //Console.ReadLine();
-
-
         }
     }
 }
